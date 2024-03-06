687a7aaa14e35c52585ee4d07565051ce38225c5
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-06 18:39:48 +0530

    Update routetable.tf

--- routetable.tf
+++ routetable.tf
@@ -1,6 +1,11 @@
 resource "aws_route_table" "route1" {
   vpc_id = aws_vpc.demovpc.id
 
+   route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igpy.id
+    }
+
   tags = {
     Name = "routetable"
   }

ab86174abe48ed75eb5e16b96592240f45b3aef2
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-06 18:41:45 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "sg1" {
   }
 
  ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

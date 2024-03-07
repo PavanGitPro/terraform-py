2f2ca28fbef44aa6f827b191f9ba4eb55a1dca6d
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-07 07:25:22 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "sg1" {
   vpc_id = aws_vpc.demovpc.id
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

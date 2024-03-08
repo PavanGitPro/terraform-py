b030647f3758e613fab37d95346950f505c4028c
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-08 18:28:44 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install git -y
-git clone 
-cd villa
+git clone https://github.com/PavanGitPro/fish.git
+cd fish
 pip3 install -r requirements.txt
 screen -m -d python3 app.py

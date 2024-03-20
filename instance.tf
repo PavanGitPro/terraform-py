e7a208b23b0b0400c6489748c805758f28194b6e
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-20 20:00:58 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "python" {
   ami           = "ami-0d7a109bf30624c99"
   instance_type = "t2.micro"
 
-  key_name                    = "pyt-1"
+  key_name                    = "master-slave"
   vpc_security_group_ids      = [aws_security_group.sg1.id]
   subnet_id                   = aws_subnet.publicsubnet1.id
   associate_public_ip_address = "true"
